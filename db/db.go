3553138b2e093f45c0db8ead8ade22f3e0ed7850
Devesh Anand <contactdeveshanand@gmail.com>
2024-05-05 23:49:11 +0530

    removed comments and changed server.go to main.go

--- db/db.go
+++ db/db.go
@@ -8,18 +8,6 @@ import (
 )
 
 func GetConnection() (*sql.DB, error) {
-	// db, err := sql.Open("mysql", os.Getenv("DSN"))
-	// if err != nil {
-	// 	log.Fatalf("failed to connect: %v", err)
-	// }
-
-	// if err := db.Ping(); err != nil {
-	// 	log.Fatalf("failed to ping: %v", err)
-	// }
-	// log.Println("Successfully connected to PlanetScale!")
-
-	// return db, nil
-
 	//connect to turso db
 	url:=os.Getenv("DSN");
 	db, err := sql.Open("libsql", url)
